a2a489b382da2c5896cdf0b288d5d38c6e9579ef
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-10-07 20:18:04 +0000

    Add seceret keyvalue and hide.

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-imfl17f1@h!=+3+p5(@pt-_9fui=nz@2dl1o7&k*qd0bh2#h@!'
+#SECRET_KEY = 'django-insecure-imfl17f1@h!=+3+p5(@pt-_9fui=nz@2dl1o7&k*qd0bh2#h@!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['http://localhost:8001/blog/','.herokuapp.com']
 
 
 # Application definition

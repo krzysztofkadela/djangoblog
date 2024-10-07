d9a938f3e45674460cc3ac4be8dff2101b351867
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-10-07 20:22:05 +0000

    Add localhost to ALLOWED_HOSTS in seting.py.

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://localhost:8001/blog/', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition

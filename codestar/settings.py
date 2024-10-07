c6dd0b2d0d0510c052bc2e8d1c4f6a003df6cc9f
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-10-07 20:19:25 +0000

    Change DEBUG to False.

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['http://localhost:8001/blog/','.herokuapp.com']
+ALLOWED_HOSTS = ['http://localhost:8001/blog/', '.herokuapp.com']
 
 
 # Application definition

fa52037f714dd8ad7982025512cb5197be6f7ca3
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-10-11 18:51:20 +0000

    generic views testing.

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=0)
+    #template_name = "post_list.html"

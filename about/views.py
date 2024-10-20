5da81560d9e5bc827375782464095e7e937754dc
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-10-20 13:07:09 +0000

    displaying message after succesfull collaborate request was send.

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import About
 from .forms import CollaborateForm
 
@@ -8,7 +9,19 @@ def about_me(request):
     """
     Renders the About page
     """
+    
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
+    if request.method == 'POST':
+        collaborate_form = CollaborateForm(request.POST)
+        
+        # Check if the form is valid
+        if collaborate_form.is_valid():
+            # If necessary, adjust data here before saving
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your collaboration request has been submitted successfully.')
+            return redirect('about')  # Redirect to the 'about' page or desired location
 
     return render(
         request,
